18795d89a33f20dbb9300dc26478f7643934d878
terence.nero@lntinfotech.com <terence.nero@lntinfotech.com>
2023-07-20 11:57:06 +0530

    Update banner

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React and CD/CD
         </a>
       </header>
     </div>
